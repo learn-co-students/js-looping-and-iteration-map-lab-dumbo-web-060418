7b6d19ea53c928a32f778eb05c2b6538e64a70d6
Indira Williams <indirajamurath@gmail.com>
2018-07-15 22:07:43 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Code your solution in this file.
+ function lowerCaseDrivers(drivers){
+  const newDrivers = drivers.map(function (driver) {return driver.toLowerCase();});
+  return newDrivers;
+ }
+
+ function splitString(string, attr1, attr2){
+   const obj = {}
+   const arr = string.split(" ");
+   obj[attr1] = arr[0];
+   obj[attr2] = arr[arr.length-1];
+   return obj;
+ }
+
+ function nameToAttributes(drivers){
+   const attrs = drivers.map(function (driver) {return splitString(driver, 'firstName', 'lastName');});
+   return attrs;
+ }
+
+ function attributesToPhrase(drivers){
+   const phrases = [];
+   for (const obj of drivers){
+     phrases.push(`${obj.name} is from ${obj.hometown}`);
+   }
+   return phrases;
+ }
